7452be0968b7e382fb80b97fc3e49ef9da2ce676
M0tgh3y <mot13832004@gmail.com>
2025-12-31 13:50:13 +0330

    last change.

--- app/src/main/java/com/example/afterreinstall/AfterStartScreen.kt
+++ app/src/main/java/com/example/afterreinstall/AfterStartScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -48,16 +49,18 @@ fun AfterStartScreen(navController: NavHostController) {
         contentAlignment = Alignment.TopCenter
     ) {
         Column (
-            modifier = Modifier.padding(25.dp),
+            modifier = Modifier
+                .padding(25.dp)
+                .verticalScroll(rememberScrollState()),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+            verticalArrangement = Arrangement.Center,
         ) {
             Box(
                 modifier = Modifier
                     .background(ResedaGreen,
                         shape = RoundedCornerShape(24.dp))
-                    .weight(1f)
-                    .fillMaxWidth(),
+                    .fillMaxWidth()
+                    .wrapContentHeight(),
                 contentAlignment = Alignment.Center
             ) {
                 Column(
@@ -81,14 +84,13 @@ fun AfterStartScreen(navController: NavHostController) {
                 modifier = Modifier
                     .background(ResedaGreen,
                         shape = RoundedCornerShape(24.dp))
-                    .weight(5.5f)
-                    .fillMaxWidth(),
+                    .fillMaxWidth()
+                    .wrapContentHeight(),
                 contentAlignment = Alignment.Center
             ) {
                 Column(
                     modifier = Modifier
-                        .padding(25.dp)
-                        .verticalScroll(rememberScrollState()),
+                        .padding(25.dp),
                     horizontalAlignment = Alignment.CenterHorizontally,
                     verticalArrangement = Arrangement.Center,
 
@@ -275,6 +277,7 @@ fun AfterStartScreen(navController: NavHostController) {
                     }
                 }
             }
+            Spacer(modifier = Modifier.height(80.dp))
         }
     }
 }

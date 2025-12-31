d1c919b9913c61ab44f5ce7d72deab7102fba817
M0tgh3y <mot13832004@gmail.com>
2025-12-31 12:16:02 +0330

    change their styles.

--- app/src/main/java/com/example/afterreinstall/AddAListScreen.kt
+++ app/src/main/java/com/example/afterreinstall/AddAListScreen.kt
@@ -1,20 +1,36 @@
 package com.example.afterreinstall
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.afterreinstall.ui.theme.FernGreen
+import com.example.afterreinstall.ui.theme.PakistanGreen
+import com.example.afterreinstall.ui.theme.ResedaGreen
 
 
 @Composable
@@ -22,31 +38,95 @@ fun AddAListScreen(onClick: () -> Unit) {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(top = 25.dp),
-        contentAlignment = Alignment.TopCenter
+            .background(PakistanGreen),
+        contentAlignment = Alignment.Center
     ) {
-        Column (
-            modifier = Modifier.padding(start = 25.dp, end = 25.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+        Column(
+            modifier = Modifier.padding(25.dp)
         ) {
-            Text("Add a list Page")
+            Box(
+                modifier = Modifier
+                    .background(ResedaGreen,
+                        shape = RoundedCornerShape(24.dp),
+                    ),
+            ){
+                Column (
+                    modifier = Modifier.padding(25.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        modifier = Modifier.fillMaxWidth(),
+                        text = "List Name:",
+                        style = TextStyle(
+                            fontSize = 22.sp
+                        ),
+                        color = Color.White
+                    )
 
-            OutlinedTextField(
-                state = rememberTextFieldState(),
-                placeholder = { Text("Class 1") },
-                label = { Text("Add a name for list") }
-            )
+                    Spacer(modifier = Modifier.height(10.dp))
 
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Button(onClick = { onClick() }) {
-                    Text("Submit")
-                }
+                    OutlinedTextField(
+                        state = rememberTextFieldState(),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(60.dp),
+                        shape = RoundedCornerShape(24.dp),
+                        colors = OutlinedTextFieldDefaults.colors(
+                            focusedBorderColor = Color.White,
+                            unfocusedBorderColor = Color.White,
+                            focusedLabelColor = Color.White,
+                            unfocusedLabelColor = Color.White,
+                            cursorColor = Color.White
+                        ),
+                    )
 
-                OutlinedButton(onClick = { onClick() }) {
-                    Text("Cancel")
+                    Spacer(modifier = Modifier.height(20.dp))
+
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Button(
+                            onClick = { onClick() },
+                            modifier = Modifier
+                                .weight(1f)
+                                .height(60.dp),
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = FernGreen,
+                                contentColor = Color.Black
+                            )
+                        ) {
+                            Text("Submit",
+                                style = TextStyle(
+                                    fontSize = 20.sp,
+                                ),
+                                color = Color.White
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.width(20.dp))
+
+                        OutlinedButton(
+                            onClick = { onClick() },
+                            modifier = Modifier
+                                .weight(1f)
+                                .height(60.dp),
+                            border = BorderStroke(1.dp, Color.White)
+                        ) {
+                            Text("Cancel",
+                                style = TextStyle(
+                                    fontSize = 20.sp,
+                                ),
+                                color = Color.White
+                            )
+                        }
+                    }
                 }
             }
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AddAListScreenPre() {
+    AddAListScreen(onClick = {})
 }
--- app/src/main/java/com/example/afterreinstall/AddAPersonScreen.kt
+++ app/src/main/java/com/example/afterreinstall/AddAPersonScreen.kt
@@ -1,5 +1,7 @@
 package com.example.afterreinstall
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -7,90 +9,266 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.afterreinstall.ui.theme.FernGreen
+import com.example.afterreinstall.ui.theme.PakistanGreen
+import com.example.afterreinstall.ui.theme.ResedaGreen
 
 @Composable
 fun AddAPersonScreen(onClick: () -> Unit) {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(top = 25.dp),
-        contentAlignment = Alignment.TopCenter
+            .background(PakistanGreen),
+        contentAlignment = Alignment.Center
     ) {
-        Column (
-            modifier = Modifier.padding(start = 25.dp, end = 25.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+        Column(
+            modifier = Modifier.padding(25.dp)
         ) {
-            Text("Add a person screen")
+            Box(
+                modifier = Modifier
+                    .background(ResedaGreen,
+                        shape = RoundedCornerShape(24.dp),
+                    ),
+            ) {
+                Column (
+                    modifier = Modifier.padding(25.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        modifier = Modifier.fillMaxWidth(),
+                        text = "First name and last name:",
+                        style = TextStyle(
+                            fontSize = 22.sp
+                        ),
+                        color = Color.White
+                    )
 
-            OutlinedTextField(
-                state = rememberTextFieldState(),
-                placeholder = { Text("") },
-                label = { Text("First and last name") },
-                modifier = Modifier.fillMaxWidth()
-            )
+                    Spacer(modifier = Modifier.height(10.dp))
 
-            OutlinedTextField(
-                state = rememberTextFieldState(),
-                placeholder = { Text("") },
-                label = { Text("Student number") },
-                modifier = Modifier.fillMaxWidth()
-            )
+                    OutlinedTextField(
+                        state = rememberTextFieldState(),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(60.dp),
+                        shape = RoundedCornerShape(24.dp),
+                        colors = OutlinedTextFieldDefaults.colors(
+                            focusedBorderColor = Color.White,
+                            unfocusedBorderColor = Color.White,
+                            focusedLabelColor = Color.White,
+                            unfocusedLabelColor = Color.White,
+                            cursorColor = Color.White
+                        ),
+                    )
 
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                OutlinedTextField(
-                    state = rememberTextFieldState(),
-                    placeholder = { Text("") },
-                    label = { Text("Height") },
-                    modifier = Modifier.weight(1f)
-                )
+                    Spacer(modifier = Modifier.height(20.dp))
 
-                Spacer(modifier = Modifier.width(20.dp))
+                    Text(
+                        modifier = Modifier.fillMaxWidth(),
+                        text = "Student number:",
+                        style = TextStyle(
+                            fontSize = 22.sp
+                        ),
+                        color = Color.White
+                    )
 
-                OutlinedTextField(
-                    state = rememberTextFieldState(),
-                    placeholder = { Text("") },
-                    label = { Text("Weight") },
-                    modifier = Modifier.weight(1f)
-                )
+                    Spacer(modifier = Modifier.height(10.dp))
 
-                Spacer(modifier = Modifier.width(20.dp))
+                    OutlinedTextField(
+                        state = rememberTextFieldState(),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(60.dp),
+                        shape = RoundedCornerShape(24.dp),
+                        colors = OutlinedTextFieldDefaults.colors(
+                            focusedBorderColor = Color.White,
+                            unfocusedBorderColor = Color.White,
+                            focusedLabelColor = Color.White,
+                            unfocusedLabelColor = Color.White,
+                            cursorColor = Color.White
+                        ),
+                    )
 
-                OutlinedTextField(
-                    state = rememberTextFieldState(),
-                    placeholder = { Text("") },
-                    label = { Text("Age") },
-                    modifier = Modifier.weight(1f)
-                )
-            }
+                    Spacer(modifier = Modifier.height(20.dp))
 
-            OutlinedTextField(
-                state = rememberTextFieldState(),
-                placeholder = { Text("") },
-                label = { Text("ID number") },
-                modifier = Modifier.fillMaxWidth()
-            )
+                    Row(verticalAlignment = Alignment.CenterVertically) {
 
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Button(onClick = { onClick() }, modifier = Modifier.weight(1f)) {
-                    Text("Submit")
-                }
+                        Column(
+                            modifier = Modifier.weight(1f)
+                        ) {
+                            Text(
+                                text = "Height:",
+                                style = TextStyle(
+                                    fontSize = 22.sp
+                                ),
+                                color = Color.White
+                            )
 
-                OutlinedButton(onClick = { onClick() }, modifier = Modifier.weight(1f)) {
-                    Text("Cancel")
+                            Spacer(modifier = Modifier.height(10.dp))
+
+                            OutlinedTextField(
+                                state = rememberTextFieldState(),
+                                shape = RoundedCornerShape(24.dp),
+                                colors = OutlinedTextFieldDefaults.colors(
+                                    focusedBorderColor = Color.White,
+                                    unfocusedBorderColor = Color.White,
+                                    focusedLabelColor = Color.White,
+                                    unfocusedLabelColor = Color.White,
+                                    cursorColor = Color.White
+                                )
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.width(20.dp))
+
+                        Column(
+                            modifier = Modifier.weight(1f)
+                        ) {
+                            Text(
+                                text = "Width:",
+                                style = TextStyle(
+                                    fontSize = 22.sp
+                                ),
+                                color = Color.White
+                            )
+
+                            Spacer(modifier = Modifier.height(10.dp))
+
+                            OutlinedTextField(
+                                state = rememberTextFieldState(),
+                                shape = RoundedCornerShape(24.dp),
+                                colors = OutlinedTextFieldDefaults.colors(
+                                    focusedBorderColor = Color.White,
+                                    unfocusedBorderColor = Color.White,
+                                    focusedLabelColor = Color.White,
+                                    unfocusedLabelColor = Color.White,
+                                    cursorColor = Color.White
+                                )
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.width(20.dp))
+
+                        Column(
+                            modifier = Modifier.weight(1f)
+                        ) {
+                            Text(
+                                text = "Age:",
+                                style = TextStyle(
+                                    fontSize = 22.sp
+                                ),
+                                color = Color.White
+                            )
+
+                            Spacer(modifier = Modifier.height(10.dp))
+
+                            OutlinedTextField(
+                                state = rememberTextFieldState(),
+                                shape = RoundedCornerShape(24.dp),
+                                colors = OutlinedTextFieldDefaults.colors(
+                                    focusedBorderColor = Color.White,
+                                    unfocusedBorderColor = Color.White,
+                                    focusedLabelColor = Color.White,
+                                    unfocusedLabelColor = Color.White,
+                                    cursorColor = Color.White
+                                )
+                            )
+                        }
+                    }
+
+                    Spacer(modifier = Modifier.height(20.dp))
+
+                    Text(
+                        modifier = Modifier.fillMaxWidth(),
+                        text = "ID number:",
+                        style = TextStyle(
+                            fontSize = 22.sp
+                        ),
+                        color = Color.White
+                    )
+
+                    Spacer(modifier = Modifier.height(10.dp))
+
+                    OutlinedTextField(
+                        state = rememberTextFieldState(),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(60.dp),
+                        shape = RoundedCornerShape(24.dp),
+                        colors = OutlinedTextFieldDefaults.colors(
+                            focusedBorderColor = Color.White,
+                            unfocusedBorderColor = Color.White,
+                            focusedLabelColor = Color.White,
+                            unfocusedLabelColor = Color.White,
+                            cursorColor = Color.White
+                        ),
+                    )
+
+                    Spacer(modifier = Modifier.height(20.dp))
+
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Button(
+                            onClick = { onClick() },
+                            modifier = Modifier
+                                .weight(1f)
+                                .height(60.dp),
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = FernGreen,
+                                contentColor = Color.Black
+                            )
+                        ) {
+                            Text("Submit",
+                                style = TextStyle(
+                                    fontSize = 20.sp,
+                                ),
+                                color = Color.White
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.width(20.dp))
+
+                        OutlinedButton(
+                            onClick = { onClick() },
+                            modifier = Modifier
+                                .weight(1f)
+                                .height(60.dp),
+                            border = BorderStroke(1.dp, Color.White)
+                        ) {
+                            Text("Cancel",
+                                style = TextStyle(
+                                    fontSize = 20.sp,
+                                ),
+                                color = Color.White
+                            )
+                        }
+                    }
                 }
             }
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AddAPersonScreenPre() {
+    AddAPersonScreen(onClick = {})
 }
